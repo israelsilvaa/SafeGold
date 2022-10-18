754f11ab976bd6d29bb66d640056700d30cde355
Mateus Melo <Israel524.is@gmail.com>
2022-10-18 11:31:16 -0300

    implementação da tela de login

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SafeGold</title>
+    <link href="/SafeGold/css/app.css" rel="stylesheet" type="text/css">
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+        crossorigin="anonymous"
+    >
+</head>
+<body class="bg-dark">
+
+    <div class="container mt-4 ">
+        <div class="row align-items-center">
+            <div class="col-md-10 mx-auto col-lg-5">  
+                <form class="p-5 p-md-5 border rounded-3 bg-light" method="" action="">
+                    <img src="/SafeGold/assets/logo.png" class="bg"  alt="logo moeda" />
+                    <h4 class="text-center mt-5">Login</h4>
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="inputEmail" placeholder="joao@gmail.com"></input>
+                        <label for="inputEmail" >Email</label>
+                    </div> 
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="inputPassword" placeholder="senha"></input>
+                        <label for="password" >Senha</label>
+                    </div> 
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" value="lembrar de mim">Lembrar de mim
+                        </label>
+                    </div>
+                    <i class="fa-regular fa-envelope"></i>
+                    <div>
+                        <bottom class="btn btn-lg btn-success" type="submit">Entrar</bottom>
+                    </div>
+                </form>  
+            </div>
+        </div>
+    </div>
+    
+
+
+
+    <script 
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" 
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" 
+        crossorigin="anonymous"
+    ></script>
+   
+    <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" 
+        integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" 
+        crossorigin="anonymous"
+    ></script>
+</body>
+</html>
--- painel.php
+++ painel.php
@@ -4,16 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SafeGold-HOME</title>
-  <link rel="stylesheet" href="css/main.css">
+    <title>Painel</title>
+    <link href="/SafeGold/css/app.css" rel="stylesheet" type="text/css">
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+        crossorigin="anonymous"
+    >
 </head>
-<body>
+<body class="bg-dark">
 
     <div id="header">
         <div class="container">   
             <nav class="navbar navbar-expand-lg bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Safe Gold</a>
+                    <a class="navbar-brand" href="/SafeGold/index.php">
+                        <img src="/SafeGold/assets/logo.png" 
+                            class="bg-painel" alt="logo moeda"
+                        />Safe Gold
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -23,7 +33,7 @@
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pagamento.html">Pagamento</a>
+                            <a class="nav-link" href="#">Pagamento</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Transferência(PIX)</a>
@@ -33,7 +43,7 @@
                         </li>
                         
                         <li class="nav-item">
-                            <a class="nav-link" href="/login.html">SAIR</a>
+                            <a class="nav-link " href="#">SAIR</a>
                         </li>    
                         </ul>
                         
