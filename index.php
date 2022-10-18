69530309383fd753bc4a909f70d617c331ad7c04
israel silva <Israel524.is@gmail.com>
2022-10-18 19:54:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,33 +10,30 @@
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
         rel="stylesheet" 
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
-        crossorigin="anonymous"
-    >
+        crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 <body class="bg-dark">
 
     <div class="container mt-4 ">
         <div class="row align-items-center">
-            <div class="col-md-10 mx-auto col-lg-5">  
+            <div class="col-md-6 mx-auto col-lg-5">  
                 <form class="p-5 p-md-5 border rounded-3 bg-light" method="" action="">
-                    <img src="/SafeGold/assets/logo.png" class="bg"  alt="logo moeda" />
-                    <h4 class="text-center mt-5">Login</h4>
+                    <div class="text-center">
+                        <i class="fs-1 text-warning bi bi-coin  "> </i>
+                    </div>
+                    
+                    <h2 class="text-center mt-2">Safe Gold</h2>
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="inputEmail" placeholder="joao@gmail.com"></input>
-                        <label for="inputEmail" >Email</label>
+                        <input type="text" class="form-control" id="inputCpf" placeholder="CPF"></input>
+                        <label for="inputCpf" >CPF</label>
                     </div> 
                     <div class="form-floating mb-3">
                         <input type="password" class="form-control" id="inputPassword" placeholder="senha"></input>
                         <label for="password" >Senha</label>
                     </div> 
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" value="lembrar de mim">Lembrar de mim
-                        </label>
-                    </div>
-                    <i class="fa-regular fa-envelope"></i>
-                    <div>
-                        <bottom class="btn btn-lg btn-success" type="submit">Entrar</bottom>
+                    <div class="text-center">
+                        <a class="btn btn-lg btn-warning" href="/painel.html"type="submit">Entrar</a>
                     </div>
                 </form>  
             </div>
